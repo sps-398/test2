29fbbd17da38269d81e0234b4ea5df987a6e13b5
sps-398 <shashankpratapsingh5004@gmail.com>
2023-02-25 16:27:13 +0530

    Added localstorage

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('i dont love git');
+const form = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter the fields';
+
+    setTimeout(() => {
+      msg.remove();
+    }, 3000);
+  }
+  else {
+        localStorage.setItem(nameInput.value, emailInput.value);
+  }
+}
