b750c63a332afc7462c3dec83fbf2836a8706f5c
sps-398 <shashankpratapsingh5004@gmail.com>
2023-02-25 18:44:47 +0530

    Added functionality to store multiple users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
 const msg = document.querySelector('.msg');
 
 form.addEventListener('submit', onSubmit);
@@ -19,9 +20,27 @@ function onSubmit(e) {
   else {
         let user = {
           name: nameInput.value,
-          email: emailInput.value
+          email: emailInput.value,
+          phone: phoneInput.value
         }
 
-        localStorage.setItem("users", JSON.stringify(user));
+        let db = localStorage.getItem('db');
+        
+        if(db != null) {
+          const users = JSON.parse(db).users;
+          users.push(user);
+          localStorage.setItem("db", JSON.stringify({ users: users }));
+        }
+        else {
+          const users = { users: [user] };
+          localStorage.setItem("db", JSON.stringify(users));
+        }
+
+        const ul = document.getElementById('users');
+        const li = document.createElement('li');
+        li.className = 'item';
+        const userInfo = `name: ${nameInput.value}, email: ${emailInput.value}, phone: ${phoneInput.value}`;
+        li.appendChild(document.createTextNode(userInfo));
+        ul.appendChild(li);
   }
 }
